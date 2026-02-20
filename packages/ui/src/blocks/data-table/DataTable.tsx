9218ed71437506b161147fbf390799903f15037c
Doug Banks <doug@douglasebanks.com>
2026-02-20 13:11:01 -0500

    ui: stabilize sticky row separators with explicit border-bottom

--- packages/ui/src/blocks/data-table/DataTable.tsx
+++ packages/ui/src/blocks/data-table/DataTable.tsx
@@ -910,7 +910,9 @@ export function DataTable<TData, TValue>({
                                     opacity: 1,
                                     backgroundClip: "padding-box",
                                     boxShadow:
-                                      "inset 0 0 0 9999px hsl(var(--background)), inset -1px 0 0 hsl(var(--border)), inset 0 -1px 0 hsl(var(--border))",
+                                      "inset 0 0 0 9999px hsl(var(--background)), inset -1px 0 0 hsl(var(--border))",
+                                    borderBottom:
+                                      "1px solid hsl(var(--border))",
                                     isolation: "isolate" as const,
                                     contain: "paint" as const,
                                     backfaceVisibility: "hidden" as const,
@@ -1264,7 +1266,9 @@ export function DataTable<TData, TValue>({
                                         opacity: 1,
                                         backgroundClip: "padding-box",
                                         boxShadow:
-                                          "inset 0 0 0 9999px hsl(var(--background)), inset -1px 0 0 hsl(var(--border)), inset 0 -1px 0 hsl(var(--border))",
+                                          "inset 0 0 0 9999px hsl(var(--background)), inset -1px 0 0 hsl(var(--border))",
+                                        borderBottom:
+                                          "1px solid hsl(var(--border))",
                                         isolation: "isolate" as const,
                                         contain: "paint" as const,
                                         backfaceVisibility: "hidden" as const,
